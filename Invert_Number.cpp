e2fa8420ab7286fc65ea5a6d01d16d03dd9267fb
AcridGuedes <53222754+AcridGuedes@users.noreply.github.com>
2022-11-16 08:58:57 +0000

    Create Invert_Number.cpp

---
+++ Invert_Number.cpp
@@ -0,0 +1,17 @@
+/*Escreva um algoritmo em psedocodigo ou c++ que leia um número inteiro, de qualquer dimensão, e inverta os seus algoritmos.*/
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main() {
+	int num, resto = 0, final = 0;
+	cout <<"Escolha o numero!\n> ";
+	cin >> num;
+	while (num > 0) {
+		resto = num % 10;
+		num = num / 10;
+		final = final * 10 + resto;
+	}
+	cout <<"O numero escolhido invertido e = "<< final;
+
+	return 0;
+}
