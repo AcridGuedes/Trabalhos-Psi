ee1af76729548b8e5992f5121d4eb7df3c28d61d
AcridFak3 <53222754+AcridGuedes@users.noreply.github.com>
2023-03-12 22:17:55 +0000

    Update ez.cpp

--- ez.cpp
+++ ez.cpp
@@ -1,3 +1,16 @@
+/**
+Escreva um programa C++ que seja capaz de ler uma matriz K de 4x4 números 
+inteiros e some todos os números de cada linha no elemento da diagonal, colocando
+os restantes elementos a zero.
+         coluna 0   1   2   3               coluna 0   1   2   3
+            :----------------                  :----------------
+ex:  linha 0:   1   2   3   4           linha 0:   10  0   0   0
+     linha 1:   5   6   7   8     --->  linha 1:   0  26   0   0
+     linha 2:   9   0   1   2           linha 2:   0   0   12  0
+     linha 3:   3   4   5   6           linha 3:   0   0   0   18
+
+***/
+
 #include <iostream>
 
 using namespace std;
