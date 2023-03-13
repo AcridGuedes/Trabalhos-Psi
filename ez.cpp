2ed93d2fec7bf124dec0c9a61b344660e8b88cf4
AcridFak3 <53222754+AcridGuedes@users.noreply.github.com>
2023-03-13 08:56:15 +0000

    Update ez.cpp

--- ez.cpp
+++ ez.cpp
@@ -5,10 +5,9 @@ os restantes elementos a zero.
          coluna 0   1   2   3               coluna 0   1   2   3
             :----------------                  :----------------
 ex:  linha 0:   1   2   3   4           linha 0:   10  0   0   0
-     linha 1:   5   6   7   8     --->  linha 1:   0  26   0   0
+     linha 1:   5   6   7   8    --->   linha 1:   0  26   0   0
      linha 2:   9   0   1   2           linha 2:   0   0   12  0
      linha 3:   3   4   5   6           linha 3:   0   0   0   18
-
 ***/
 
 #include <iostream>
@@ -17,22 +16,22 @@ using namespace std;
 
 int main()
 {
-   int m[4][4]={{1,2,3,4},{5,6,7,8},{9,0,1,2},{3,4,5,6}}, v[4][4]={{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}}, soma = 0, total = 0, s = 0;
+   int m[4][4]={{1,2,3,4},{5,6,7,8},{9,0,1,2},{3,4,5,6}}, total = 0, s = 0;
 
-    for (int i = 0; i<4; i++){
-        for (int j = 0; j < 4; j++){
-          total += soma + m[i][j]; 
+    for (int i = 0; i<4; i++){  
+        for (int j = 0; j < 4; j++){  
+          total += m[i][j]; 
+          m[i][j] = 0;
         }
-        v[0 + s][0 + s] = total;
+        m[s][s] = total;
         total = 0;
         s++;
     }
      for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
-            cout<<v[i][j]<<"   ";
+            cout<<m[i][j]<<"   ";
         }
         cout<<endl;
      }
-
     return 0;
 }
